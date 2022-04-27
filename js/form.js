b4762a8246fed94deaa7b60aa21931f8d910d5ce
danheon kim <eksgjs3847@naver.com>
2022-04-27 13:24:20 +0900

    Implement async await

--- js/form.js
+++ js/form.js
@@ -1,50 +1,40 @@
 let nextId = 4;
 let selectedId = null;
 
-// TODO: 중복되는 코드 정리
-// TODO: toggleFormClass 다시 짜기
-
 // Delete Item
-function deleteItem() {
-  fetch(`http://localhost:3000/topics/${selectedId}`, {
+async function deleteItem() {
+  const response = await fetch(`http://localhost:3000/topics/${selectedId}`, {
     method: "delete",
-  })
-    .then((res) => res.json())
-    .then(() => {
-      selectedId = null;
-      render();
-      handleTitle();
-    });
+  });
+  selectedId = null;
+  render();
+  handleTitle();
 }
 
 // Update contents
-function handleUpdate() {
+async function handleUpdate() {
   const title = $(".input-title").value;
   const body = $(".input-desc").value;
-  fetch(`http://localhost:3000/topics/${selectedId}`, {
+  const response = await fetch(`http://localhost:3000/topics/${selectedId}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ title: title, body: body }),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      render();
-      toggleFormClass(".update-form");
-      selectedId = data.id;
-    });
+  });
+  const data = await response.json();
+  render();
+  toggleFormClass(".update-form");
+  selectedId = data.id;
 }
 
 // Update form toggle
-function toggleUpdate() {
-  fetch(`http://localhost:3000/topics/${selectedId}`)
-    .then((response) => response.json())
-    .then((topics) => {
-      toggleFormClass(".update-form");
-      $("form input").value = topics.title;
-      $("form textarea").innerText = topics.title;
-    });
+async function toggleUpdate() {
+  const response = await fetch(`http://localhost:3000/topics/${selectedId}`);
+  const topics = await response.json();
+  toggleFormClass(".update-form");
+  $("form input").value = topics.title;
+  $("form textarea").innerText = topics.title;
 }
 
 // Show update & delete buttons
@@ -67,61 +57,60 @@ function handleTitle(e) {
   $(".article").innerHTML = `<h2>Welcome</h2><p>Hello, WEB!!</p>`;
   selectedId = null;
   handleButtons();
+  $$("form").forEach((form) => {
+    form.classList.add("hidden");
+  });
 }
 
 // Show contents of menu
-function handleMenu(e) {
+async function handleMenu(e) {
   e.preventDefault();
-  fetch("http://localhost:3000/topics/")
-    .then((response) => response.json())
-    .then((topics) => {
-      topics.forEach((el) => {
-        selectedId = parseInt(e.target.id);
-        if (selectedId === el.id) {
-          $(".article h2").innerText = el.title;
-          $(".article p").innerText = el.body;
-        }
-        handleButtons();
-      });
-    });
+  const response = await fetch("http://localhost:3000/topics/");
+  const topics = await response.json();
+  topics.forEach((el) => {
+    selectedId = parseInt(e.target.id);
+    if (selectedId === el.id) {
+      $(".article h2").innerText = el.title;
+      $(".article p").innerText = el.body;
+    }
+    handleButtons();
+  });
 }
 
 // Render list item
-function render() {
-  fetch("http://localhost:3000/topics")
-    .then((response) => response.json())
-    .then((topics) => {
-      // 받아온 json 데이터를 사용한다!
-      // 데이터를 받은 후에 호출된다.
-      $("nav ol").innerText = "";
-      topics.forEach((el) => {
-        const li = document.createElement("li");
-        li.innerHTML = `<a id="${el.id}" href="/read/${el.id}">${el.title}</a>`;
-        $(".menu-list").appendChild(li);
-        initEventHandlers();
-      });
-    });
+async function render() {
   $("nav ol").innerText = "Loading...";
+
+  const response = await fetch("http://localhost:3000/topics");
+  const topics = await response.json();
+  $("nav ol").innerText = "";
+  topics.forEach((el) => {
+    const li = document.createElement("li");
+    li.innerHTML = `<a id="${el.id}" href="/read/${el.id}">${el.title}</a>`;
+    $(".menu-list").appendChild(li);
+  });
+  initEventHandlers();
+  $$("form").forEach((form) => {
+    form.classList.add("hidden");
+  });
 }
 
 // Add list item
-function handleCreateItem(e) {
+async function handleCreateItem(e) {
   e.preventDefault();
   const title = $(".input-title").value;
   const body = $(".input-desc").value;
-  fetch("http://localhost:3000/topics", {
+  const response = await fetch("http://localhost:3000/topics", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ title: title, body: body }),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      render();
-      toggleFormClass(".create-form");
-      selectedId = data.id;
-    });
+  });
+  const data = await res.json();
+  render();
+  toggleFormClass(".create-form");
+  selectedId = data.id;
 }
 
 // Show form
