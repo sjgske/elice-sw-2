31168df410ac17f1aed5bfc3fe3d0a269933cc4a
danheon kim <eksgjs3847@naver.com>
2022-05-18 11:13:42 +0900

    controller

--- 06/express/myapp/routes/post.js
+++ 06/express/myapp/routes/post.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const BookSchema = require("../models/book");
+const BookSchema = require("../models/book"); // controller 작성 -> 지워도 무방
+const bookController = require("../controller/post");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -10,20 +11,8 @@ router.get("/del", (req, res) => {
   res.render("delete");
 });
 
-// 데이터 찾기
-router.get("/bookinfo/:id", (req, res) => {
-  const author = req.params.id;
-
-  // Model.find(찾아올 데이터, callback);
-  // findOne -> 하나만 찾기
-  BookSchema.find({ author: author })
-    .then((result) => {
-      res.json(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+// 데이터 찾기 (controller 적용)
+router.get("/bookinfo/:id", bookController.getbookinfo);
 
 // 삭제하기
 router.post("/del/:id", (req, res) => {
@@ -38,6 +27,7 @@ router.post("/del/:id", (req, res) => {
     });
 });
 
+// 데이터 저장 (controller 적용)
 router.post("/", (req, res, next) => {
   const name = req.body.name;
   const phone = req.body.phone;
@@ -46,22 +36,6 @@ router.post("/", (req, res, next) => {
   // next();
 });
 
-router.post("/addbook", (req, res) => {
-  const title = req.body.title;
-  const author = req.body.author;
-  const price = req.body.price;
-  const date = req.body.date;
-
-  // 요청한 데이터를 BookSchema에 할당해준다.
-  let bookData = new BookSchema({
-    title: title,
-    author: author,
-    price: price,
-    date: date,
-  });
-
-  bookData.save(); // 데이터 저장
-  res.redirect("/expost"); // 리로드
-});
+router.post("/addbook", bookController.addbook);
 
 module.exports = router;
