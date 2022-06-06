f0f3ea57aad4d7cf683ce12cd0a365df2ad078d4
Danheon <eksgjs3847@gmail.com>
2022-06-06 14:49:57 +0900

    Props

--- 03/src/App.js
+++ 03/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-function HeaderTag() {
+function Header() {
   return (
     <header>
       <h1>
@@ -10,26 +10,47 @@ function HeaderTag() {
   );
 }
 
+function Nav({ data }) {
+  const list = data.map((el) => (
+    <li key={el.id}>
+      <a href={`/read/${el.id}`}>{el.title}</a>
+    </li>
+  ));
+  // const list = [
+  //   <li>
+  //     <a href="/read/1">html</a>
+  //   </li>,
+  //   <li>
+  //     <a href="/read/2">css</a>
+  //   </li>,
+  // ];
+  return (
+    <nav>
+      <ol>{list}</ol>
+    </nav>
+  );
+}
+
+function Article({ title, body }) {
+  return (
+    <article>
+      <h2>{title}</h2>
+      {body}
+    </article>
+  );
+}
+
 function App() {
+  const topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+  ];
   return (
     <div>
-      <HeaderTag />
-
-      <nav>
-        <ol>
-          <li>
-            <a href="/read/1">html</a>
-          </li>
-          <li>
-            <a href="/read/2">css</a>
-          </li>
-        </ol>
-      </nav>
-
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB!
-      </article>
+      <Header />
+      <Nav data={topics} />
+      <Article title="Welcome" body="Hello, WEB!" />
+      <Article title="HTML" body="HTML is ..." />
     </div>
   );
 }
